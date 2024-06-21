13f7be02822da22b17844c2425fecfcf11471709
yangxu52 <yyswj10@163.com>
2024-06-21 16:09:00 +0800

    feat: support https

--- src/app.js
+++ src/app.js
@@ -1,19 +1,28 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import https from 'node:https'
 import express from 'express'
-import expressWebSocket from 'express-ws'
+import expressWs from 'express-ws'
 import config from './config.js'
-import { rtspRoute } from './router/rtsp.js'
+import { rtsp2flvService } from './service/rtsp2flv.js'
 
+const __dirname = path.resolve()
+
+const options = {
+  key: fs.readFileSync(path.join(__dirname, 'src/cert', 'private.key')),
+  cert: fs.readFileSync(path.join(__dirname, 'src/cert', 'public.pem')),
+}
 // 创建服务器
 const app = express()
-expressWebSocket(app, null, { perMessageDeflate: true })
+const server = https.createServer(options, app)
+expressWs(app, server, { perMessageDeflate: false })
 
-// WebSocket 路由
-app.ws('/rtsp/', rtspRoute)
+app.ws('/rtsp/', rtsp2flvService)
 
 // 获取端口号
 const { port } = config[process.env.NODE_ENV]
 
 // 启动服务器
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
